92e1da66a47bd78ffcebc96d02baeb2a1c2d043c
Vahidpro <ivahidp@gmail.com>
2023-12-09 17:54:19 +0330

    Added max and min values from API

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -6,27 +6,31 @@ maxTempContainer = document.querySelector(".max-temp-value");
 minTempContainer = document.querySelector(".min-temp-value");
 
 const fetchWeather = async () => {
-	const res = await fetch(
-		"https://api.dastyar.io/express/weather?lat=35.67194277&lng=51.42434403&lang=fa&theme=light"
-	);
-	const data = await res.json();
-	console.log(data);
+	try {
+		// const res = await fetch(
+		// 	"https://api.dastyar.io/express/weather?lat=35.67194277&lng=51.42434403&lang=fa&theme=light"
+		// );
+		const data = await res.json();
+		console.log(data);
 
-	currentWeatherContainer.innerHTML = `${data[0].current
-		.toLocaleString("fa-IR")
-		.slice(0, 2)}°`;
+		currentWeatherContainer.innerHTML = `${data[0].current
+			.toLocaleString("fa-IR")
+			.slice(0, 2)}°`;
 
-	currentWeatherIconContainer.innerHTML = `<img src="https://openweathermap.org/img/wn/${data[0].weather.icon}@2x.png"/>`;
+		currentWeatherIconContainer.innerHTML = `<img src="https://openweathermap.org/img/wn/${data[0].weather.icon}@2x.png"/>`;
 
-	weatherDescriptionContainer.innerHTML = data[0].customDescription.text;
+		weatherDescriptionContainer.innerHTML = data[0].customDescription.text;
 
-	weatherDescriptionEmojiContainer.innerHTML = data[0].customDescription.emoji;
+		weatherDescriptionEmojiContainer.innerHTML = data[0].customDescription.emoji;
 
-	maxTempContainer.innerHTML = `${data[0].max
-		.toLocaleString("fa-IR")
-		.slice(0, 2)}°`;
-	minTempContainer.innerHTML = `${data[0].min
-		.toLocaleString("fa-IR")
-		.slice(0, 2)}°`;
+		maxTempContainer.innerHTML = `${data[0].max
+			.toLocaleString("fa-IR")
+			.slice(0, 2)}°`;
+		minTempContainer.innerHTML = `${data[0].min
+			.toLocaleString("fa-IR")
+			.slice(0, 2)}°`;
+	} catch (error) {
+		throw new Error(error);
+	}
 };
 fetchWeather();
