abcd2ad8c903fd8b2b22aabfd19a69bb42bb69ea
Vahidpro <ivahidp@gmail.com>
2023-12-09 11:07:15 +0330

    Added persian date from system

--- assets/scripts/timeAndDate.js
+++ assets/scripts/timeAndDate.js
@@ -1,4 +1,5 @@
 timeContainer = document.querySelector(".time");
+persianDateContainer = document.querySelector(".persian-date");
 
 function setTime() {
 	let date = new Date();
@@ -8,5 +9,23 @@ function setTime() {
 	timeContainer.innerHTML = `${hours}:${minutes}`;
 }
 
+function setPersianDate() {
+	const today = new Date();
+
+	const options = {
+		calendar: "persian",
+		month: "long",
+		day: "numeric",
+	};
+
+	const persianDate = new Intl.DateTimeFormat("fa-IR", options).format(today);
+
+	persianDateContainer.innerHTML = `${persianDate}`;
+}
+
+setPersianDate();
+
 setTime();
 setInterval(setTime, 1000);
+
+// Test area
