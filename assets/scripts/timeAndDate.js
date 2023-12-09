fa4d089d312ca538607ee5236f71d49c2089065e
Vahidpro <ivahidp@gmail.com>
2023-12-09 11:11:50 +0330

    Fixed minutes to always have two digits

--- assets/scripts/timeAndDate.js
+++ assets/scripts/timeAndDate.js
@@ -4,7 +4,9 @@ persianDateContainer = document.querySelector(".persian-date");
 function setTime() {
 	let date = new Date();
 	let hours = date.getHours().toLocaleString("fa-IR");
-	let minutes = date.getMinutes().toLocaleString("fa-IR");
+	let minutes = date.getMinutes().toLocaleString("fa-IR", {
+		minutes: "2-digit",
+	});
 
 	timeContainer.innerHTML = `${hours}:${minutes}`;
 }
@@ -24,6 +26,7 @@ function setPersianDate() {
 }
 
 setPersianDate();
+setInterval(setPersianDate, 1000);
 
 setTime();
 setInterval(setTime, 1000);
