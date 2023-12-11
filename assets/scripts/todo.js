57abf95adf504e40a2cf23d44f8fe7edc17f6adc
Vahidpro <ivahidp@gmail.com>
2023-12-11 18:40:53 +0330

    Added transition for add and delete task

--- assets/scripts/todo.js
+++ assets/scripts/todo.js
@@ -48,6 +48,7 @@ const createTask = (taskTitle, id) => {
 	}
 	const li = document.createElement("li");
 	li.classList.add("task-item");
+	li.classList.add("transition-0");
 	li.id = id;
 	li.innerHTML = `
         <div class="done"></div>
@@ -58,6 +59,11 @@ const createTask = (taskTitle, id) => {
         </div>
     `;
 	tasksListContainer.prepend(li);
+
+	setTimeout(() => {
+		li.classList.remove("transition-0");
+		li.classList.add("transition-1");
+	}, 200);
 };
 
 function init() {
@@ -71,15 +77,21 @@ function init() {
 }
 init();
 
-function deleteTask(id) {
+function deleteTask(id, e) {
+	const li = document.getElementById(id);
+	li.classList.remove("transition-1");
+	li.classList.add("transition-0");
 	tasks = tasks.filter((task) => task.id != Number(id));
 	localStorage.setItem("tasks", JSON.stringify(tasks));
+
+	setTimeout(() => {
+		e.target.parentElement.parentElement.parentElement.remove();
+	}, 300);
 }
 
 tasksListContainer.addEventListener("click", (e) => {
 	const id = e.target.parentElement.parentElement.parentElement.id;
 	if (e.target.classList.contains("btn-delete")) {
-		deleteTask(id);
-		e.target.parentElement.parentElement.parentElement.remove();
+		deleteTask(id, e);
 	}
 });
