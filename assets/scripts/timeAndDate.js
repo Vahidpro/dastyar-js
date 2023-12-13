f655aa4c35ed9bbd37a07e565a28376ce7784cba
Vahidpro <ivahidp@gmail.com>
2023-12-13 20:26:27 +0330

    Fixed time not update correctly bug

--- assets/scripts/timeAndDate.js
+++ assets/scripts/timeAndDate.js
@@ -3,18 +3,20 @@ const persianDateContainer = document.querySelector(".persian-date");
 const gregorianDateContainer = document.querySelector(".gregorian-date");
 const islamicDateContainer = document.querySelector(".islamic-date");
 
-const today = new Date();
-
 function setTime() {
+	let today = new Date();
 	let hours = today.getHours().toLocaleString("fa-IR");
 	let minutes = today.getMinutes().toLocaleString("fa-IR", {
 		minimumIntegerDigits: 2,
 	});
 
+	console.log(hours, minutes);
+
 	timeContainer.innerHTML = `${hours}:${minutes}`;
 }
 
 function setPersianDate() {
+	let today = new Date();
 	const options = {
 		calendar: "persian",
 		month: "long",
@@ -33,6 +35,7 @@ setPersianDate();
 // setInterval(setPersianDate, 1000);
 
 const gregorianDate = () => {
+	let today = new Date();
 	const day = today.getDate().toLocaleString("fa-IR");
 	const month = today.toLocaleString("en-US", { month: "short" });
 	const year = today.getFullYear().toLocaleString("fa-IR", {
@@ -47,6 +50,7 @@ gregorianDate();
 // setInterval(gregorianDate, 1000);
 
 const islamicDate = () => {
+	let today = new Date();
 	const day = today.toLocaleString("ar-SA", {
 		day: "numeric",
 	});
